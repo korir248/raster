064f4f75528b6c517d8a4f990cac9b3e95e84c55
Eugene Korir <korir16071@gmail.com>
2025-06-16 14:25:11 +0300

    chore: add multiple images

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::path::Path;
 
 use image::{ImageBuffer, Rgb, RgbImage};
+use rand::Rng;
+
+const WIDTH: usize = 256;
+const HEIGHT: usize = 256;
+const TRIANGLE_COUNT: usize = 100;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Float3 {
@@ -34,12 +39,9 @@ impl Float3 {
     }
 }
 
-pub type Grid = [[Float3; 64]; 64];
+pub type Grid = [[Float3; 256]; 256];
 
 pub fn create_test_image() -> Grid {
-    const WIDTH: usize = 64;
-    const HEIGHT: usize = 64;
-
     let mut image: [[Float3; WIDTH]; HEIGHT] = [[Float3::new(0.0, 0.0, 0.0); WIDTH]; HEIGHT];
 
     let a = Float2::new(0.2 * WIDTH as f64, 0.2 * HEIGHT as f64);
@@ -59,11 +61,8 @@ pub fn create_test_image() -> Grid {
     image
 }
 
-pub fn save_as_bmp(image: &[[Float3; 64]; 64], path: &str) -> Result<(), image::ImageError> {
-    let width = image[0].len() as u32;
-    let height = image.len() as u32;
-
-    let mut img_buffer: RgbImage = ImageBuffer::new(width, height);
+pub fn save_as_bmp(image: &Grid, path: &str) -> Result<(), image::ImageError> {
+    let mut img_buffer: RgbImage = ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
 
     for (y, row) in image.iter().enumerate() {
         for (x, pixel) in row.iter().enumerate() {
@@ -87,4 +86,56 @@ pub fn is_point_in_triangle(p: Float2, triangle: [Float2; 3]) -> bool {
     let d3 = cross_product(c, a, p);
 
     (d1 * d2 >= 0.0) && (d2 * d3 >= 0.0)
+}
+
+pub fn render(points: &[Float2], triangle_cols: &[Float3], image: &mut Grid) -> Grid {
+    for x in 0..HEIGHT {
+        for y in 0..WIDTH {
+            for (i, chunk) in points.chunks_exact(3).enumerate() {
+                let a = chunk[0];
+                let b = chunk[1];
+                let c = chunk[2];
+
+                let p = Float2::new(x as f64, y as f64);
+
+                if is_point_in_triangle(p, (a, b, c).into()) {
+                    image[x][y] = triangle_cols[i / 3];
+                }
+            }
+        }
+    }
+    *image
+}
+
+pub fn create_test_images() -> Grid {
+    let mut rng = rand::rng();
+    let mut points = [Float2::new(0.0, 0.0); TRIANGLE_COUNT * 3];
+    let mut velocities = [Float2::new(0.0, 0.0); TRIANGLE_COUNT * 3];
+    let mut triangle_cols = [Float3::new(0.0, 0.0, 0.0); TRIANGLE_COUNT];
+
+    let mut image: [[Float3; WIDTH]; HEIGHT] = [[Float3::new(0.0, 0.0, 0.0); WIDTH]; HEIGHT];
+
+    let half_size = Float2::new(32.0, 32.0);
+
+    for point in points.iter_mut() {
+        *point = Float2::new(
+            rng.random_range(-half_size.x..half_size.x),
+            rng.random_range(-half_size.y..half_size.y),
+        );
+    }
+    for (i, chunk) in velocities.chunks_exact_mut(3).enumerate() {
+        let velocity = Float2::new(4.0, 4.0);
+
+        for v in chunk.iter_mut() {
+            *v = velocity;
+        }
+
+        triangle_cols[i] = Float3::new(
+            rng.random_range(0.0..1.0),
+            rng.random_range(0.0..1.0),
+            rng.random_range(0.0..1.0),
+        )
+    }
+
+    render(&points, &triangle_cols, &mut image)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use raster::{create_test_image, save_as_bmp};
+use raster::{create_test_image, create_test_images, save_as_bmp};
 
 fn main() {
     let image = create_test_image();
     save_as_bmp(&image, "new.bmp").unwrap();
+
+    let images= create_test_images();
+    save_as_bmp(&images, "multiple.bmp").unwrap();
 }
